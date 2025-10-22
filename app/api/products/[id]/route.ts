906bd3ac0e40b28de67d5270356f25dfc54a2544
ego5g <ego5g@users.noreply.github.com>
2025-10-22 18:08:39 +0000

    еплой# Please enter the commit message for your changes. Lines starting

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -2,6 +2,8 @@
 import { NextResponse } from 'next/server';
 import { db } from '../../../../lib/firebaseAdmin';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
     const { id } = params;
