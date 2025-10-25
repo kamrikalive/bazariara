ab0d6b0d184702ba2e79b0db2c3bf776d64e4d6d
ego5g <ego5g@users.noreply.github.com>
2025-10-25 08:29:27 +0000

    Низ

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { CartProvider } from '@/contexts/CartContext';
 import { OrderProvider } from '@/contexts/OrderContext';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export const metadata = {
   title: 'MarketGE',
@@ -14,11 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex flex-col min-h-screen">
         <OrderProvider>
           <CartProvider>
               <Header />
-              {children}
+              <main className="flex-grow">{children}</main>
+              <Footer />
           </CartProvider>
         </OrderProvider>
       </body>
