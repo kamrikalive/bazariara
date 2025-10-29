799f9d306f4ebb1aa90f17631bb9d05c0b2b8d61
ego5g <ego5g@users.noreply.github.com>
2025-10-29 10:14:25 +0000

    фикс

--- app/products/[category]/[id]/page.tsx
+++ app/products/[category]/[id]/page.tsx
@@ -56,7 +56,6 @@ export async function generateMetadata({ params }: { params: { category: string,
           alt: product.title,
         },
       ],
-      type: 'product',
       siteName: 'Bazar Iara',
     },
   };
